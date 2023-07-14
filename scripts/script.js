6afe433c963efaba92c7115c97428a4b69ff45e1
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-07-15 05:30:11 +0600

    mobile responsive added

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100vw";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
